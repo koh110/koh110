92e776447e66c99679d808a0f0f99116c3fe0a6c
koh110 <kohta110@gmail.com>
2016-04-25 01:35:29 +0900

    fix for smartphone

--- www/share/components/app.js
+++ www/share/components/app.js
@@ -2,16 +2,22 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const classNames = require('classnames');
 
 require('main.scss');
 const Contents = require('contents/contents');
 const Header = require('header/header');
 const Footer = require('footer/footer');
 
+const smartphone = /iphone|android/.test(window.navigator.userAgent.toLowerCase());
+
 const Wrapper = React.createClass({
   render() {
+    const wrapperClass = classNames('wrapper', {
+      smartphone: smartphone
+    });
     return (
-      <div className="wrapper">
+      <div className={wrapperClass}>
         <Header />
         <div className="contents-wrapper">
           <Contents />
--- www/share/components/contents/profile/profile.jsx
+++ www/share/components/contents/profile/profile.jsx
@@ -17,7 +17,7 @@ module.exports = React.createClass({
               <h2>KOHTA ITO</h2>
               <div>
                 <p>cssからフロントエンド, サーバサイド, 構成管理までWebならなんでも。</p>
-                <p>技術を使って驚かせたり楽しませたりがモットーです。</p>
+                <p>技術を使って驚かせたり楽しませたりが好きです。</p>
                 <p>JavaScript好きです。最近はNode.js書いて生きてます。</p>
               </div>
             </div>
