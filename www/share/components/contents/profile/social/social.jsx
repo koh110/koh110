c24ad90f85a9194fad95e02aab646add203d44c6
koh110 <kohta110@gmail.com>
2016-04-24 23:14:22 +0900

    fix component social-icons

--- www/share/components/contents/profile/social/social.jsx
+++ www/share/components/contents/profile/social/social.jsx
@@ -1,52 +1,50 @@
 'use strict';
 
 const React = require('react');
+const classNames = require('classnames');
 require('contents/profile/social/social.scss');
+
+const socials = [{
+  url: 'https://github.com/koh110',
+  classNames: ['fa', 'fa-github']
+}, {
+  url: 'https://www.facebook.com/kohta110',
+  classNames: ['fa', 'fa-facebook-official']
+}, {
+  url: 'https://twitter.com/koh110',
+  classNames: ['fa', 'fa-twitter']
+}, {
+  url: 'https://jp.linkedin.com/in/ito-kohta-24078410b',
+  classNames: ['fa', 'fa-linkedin-square']
+}, {
+  url: 'http://www.slideshare.net/kohta110',
+  classNames: ['fa', 'fa-slideshare']
+}, {
+  url: 'http://b.hatena.ne.jp/koh110/',
+  classNames: ['hatena']
+}, {
+  url: 'http://koh110.hatenablog.com/',
+  classNames: ['hatena-blog']
+}, {
+  url: 'http://qiita.com/koh110',
+  classNames: ['qiita']
+}];
 
 module.exports = React.createClass({
   render() {
-    const github = 'social-icon fa fa-github';
-    const facebook = 'social-icon fa fa-facebook-official';
-    const twitter = 'social-icon fa fa-twitter';
-    const linkedin = 'social-icon fa fa-linkedin-square';
-    const slideshare = 'social-icon fa fa-slideshare';
-    const hatena = 'social-icon hatena';
-    const hatenaBlog = 'social-icon hatena-blog';
-    const qiita = 'social-icon qiita';
+    const icons = socials.map((e, i) => {
+      const key = `${i}-${e.url}`;
+      const socialIcon = classNames('social-icon', ...e.classNames);
+      return (
+        <a key={key} className="social-icon-wrapper" target="_blank"
+          href={e.url}>
+          <i className={socialIcon}></i>
+        </a>
+      );
+    });
     return (
       <div className="social">
-        <a className="social-icon-wrapper" target="_blank"
-          href="https://github.com/koh110">
-          <i className={github}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="https://www.facebook.com/kohta110">
-          <i className={facebook}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="https://twitter.com/koh110">
-          <i className={twitter}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="https://jp.linkedin.com/in/ito-kohta-24078410b">
-          <i className={linkedin}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="http://www.slideshare.net/kohta110">
-          <i className={slideshare}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="http://b.hatena.ne.jp/koh110/">
-          <i className={hatena}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="http://koh110.hatenablog.com/">
-          <i className={hatenaBlog}></i>
-        </a>
-        <a className="social-icon-wrapper" target="_blank"
-          href="http://qiita.com/koh110">
-          <i className={qiita}></i>
-        </a>
+        {icons}
       </div>
     );
   }
